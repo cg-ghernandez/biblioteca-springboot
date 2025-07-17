649d952b257df574ffe0788e6beea76d2db21eb2
Gerald Hernández S <ghernandez@coopeguanacaste.com>
2025-07-16 19:05:34 -0600

    Prueba configuracion git

--- src/main/java/com/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/biblioteca/BibliotecaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BibliotecaApplication {
     public static void main(String[] args) {
+        //Este es un comentario para confirmar el git
         SpringApplication.run(BibliotecaApplication.class, args);
     }
 }
